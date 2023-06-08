b3e5f6c64a90b923daffd5b514474641bdbbfbb0
pearl <pearlgonzalez77@gmail.com>
2023-06-08 14:28:22 -0500

    authchecker

--- src/auth/AuthChecker.tsx
+++ src/auth/AuthChecker.tsx
@@ -1,27 +1,33 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { signInWithPopup } from 'firebase/auth'
-import { auth, Providers } from '../config/firebase'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, onAuthStateChanged } from 'firebase/auth';
+import { auth, Providers } from '../config/firebase';
 
 interface Props {
     children: React.ReactNode;
 }
 
-const AuthChecker = ({ children}: Props) => {
-  const navigate = useNavigate();
-  // This will just check if the user is logged in, if so, it returns the children
-  // (which are passed as props - it's just whatever component is either protected
-  // or not)
-  // otherwise it sends them to the login route
-  useEffect(() => {
-    if (!auth.currentUser) {
-        navigate("../")
-        signInWithPopup(auth, Providers.google)
+const AuthChecker =({ children }: Props) => {
+    const navigate = useNavigate();
+
+    const signInOnClick = async () => {
+        const response = await signInWithRedirect(auth, Providers.google);
     }
-  }, [])
-  return (
-    <>{children}</>
-  )
-}
+      
+    useEffect(() => {
+      const auth_state = onAuthStateChanged(auth, (user) => {
+        if (!user) {
+          signInOnClick()
+          navigate('/');
+          
+        }
+      });
+      return () => auth_state();
+    }, [auth, navigate]);
+    
+    return (
+        <>{children}</>
+      )
+    }
 
 export default AuthChecker
